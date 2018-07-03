f8251ac1cf6c2d5962c06543ebeb1be317e95c52
VP <vovvva33@gmail.com>
2018-07-03 23:11:35 +0300

    practic

--- homeWork -4/index.js
+++ homeWork -4/index.js
@@ -89,22 +89,22 @@
 
 // for(let key in hotel){
 //   console.log('key', hotel[key])
-// }
+// // }
 
-const hotel = {
-    name : "Resort Hotel",
-    stars: 5,
-    capacity: 100,
-    manager: 'petro',
-    security: 'Oleg',
-  };
+// const hotel = {
+//     name : "Resort Hotel",
+//     stars: 5,
+//     capacity: 100,
+//     manager: 'petro',
+//     security: 'Oleg',
+//   };
 
-  const objKeys = Object.keys(hotel);
-  const objValues = Object.values(hotel)
-  const objEnteries = Object.entries(hotel)
-  console.log(objKeys)
-  console.log(objValues)
-  console.log(objEnteries)
+//   const objKeys = Object.keys(hotel);
+//   const objValues = Object.values(hotel)
+//   const objEnteries = Object.entries(hotel)
+//   console.log(objKeys)
+//   console.log(objValues)
+//   console.log(objEnteries)
 
 
   // const wrp = document.querySelector(".wrapper");
@@ -114,4 +114,69 @@ const hotel = {
   //   const num = parseInt(event.target.textContent);
   //   event.target.textContent = num + 1;
   // }
+  /*  
+  Напишите скрипт, который, для объекта user, последовательно: 
   
+    - добавляет поле mood со значением 'happy'
+    
+    - заменяет значение hobby на 'javascript'
+    
+    - удаляет свойство premium
+    
+    - выводит содержимое объекта user в формате ключ:значение 
+      используя цикл for...in
+    
+    - выводит содержимое объекта user в формате ключ:значение 
+      используя Object.keys и for...of
+    
+    - выводит содержимое объекта user в формате ключ:значение 
+      используя Object.entries и for...of
+*/
+
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true
+};
+
+user.mood = 'happy';
+user.hobby = 'javascript'
+delete user.premium;
+// for(let red in user ){
+//   console.log(red);
+// }
+// const keys = Object.keys(user);
+// for (let redo of keys){
+//   console.log(user[redo])
+// }
+
+let sv = Object.entries(user)
+for (let der of sv){
+  let key = der[0];
+  let value = der[1]
+  console.log(`${key} : ${value}`)
+}
+/*
+  Напиште скрипт который определит и выведет в консоль 
+  имя сотрудника который выполнил больше всех задач.
+
+  Сотрудники и кол-во выполненых задач содержатся 
+  как свойства объекта в формате "имя":"кол-во задач"
+*/
+
+const tasksCompleted = {
+  ann: 29,
+  david: 35,
+  helen: 1,
+  lorence: 99
+};
+
+let key = Object.keys(tasksCompleted)
+
+for (let i = 1; i < key.length; i += 1){
+if (tasksCompleted[key[i]] > tasksCompleted[key[0]])
+  key[0] = key[i];
+}
+
+console.log(key[0])
